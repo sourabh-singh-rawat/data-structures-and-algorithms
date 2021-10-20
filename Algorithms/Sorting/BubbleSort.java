dac1d6a7d91f1827f618b861073475f09e1a6800
lacoste-coste <nino.chan.cc@gmail.com>
2021-10-20 23:17:17 +0530

    Bubble Sort

---
+++ Algorithms/Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1 };
+        
+        printArray(arr);
+        System.out.println("\nSorted using Bubble Sort: ");
+        bubbleSort(arr);
+        printArray(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // swap
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+}
