12ca7b8fb6154a316778b84c23029ad2996fff46
lacoste-coste <nino.chan.cc@gmail.com>
2021-10-28 08:17:46 +0530

    added function to calculate loop length

--- LinkedList/SinglyLinkedList/Easy/Loop.java
+++ LinkedList/SinglyLinkedList/Easy/Loop.java
@@ -8,7 +8,7 @@ class Loop {
 
         // 21->22->31->77->21
         list.head.next.next.next.next = list.head;
-        System.out.println(floydLoopDetection(list));
+        loopLength(list);
     }
 
     static class LinkedList {
@@ -23,6 +23,35 @@ class Loop {
         Node(int val) {
             this.val = val;
         }
+    }
+
+    static void loopLength(LinkedList list) {
+        // check if loop exist with floyd algo
+        Node curSlow = list.head, curFast = list.head;
+        while (curFast != null && curFast.next != null) {
+            curFast = curFast.next.next;
+            curSlow = curSlow.next;
+
+            if (curFast == curSlow) {
+                System.out.println("Loop Exists");
+                break;
+            }
+        }
+
+        // if loop doesn't exist
+        if (curFast != curSlow) {
+            System.out.println("Loop doesn't exists");
+            return;
+        }
+
+        // curFast = curSlow
+        int count = 1;
+        while (curSlow.next != curFast) {
+            count++;
+            curSlow = curSlow.next;
+        }
+
+        System.out.println("Loop Length: " + count);
     }
 
     static boolean checkLoop(LinkedList list) {
