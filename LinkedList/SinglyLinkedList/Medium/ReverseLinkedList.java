07d379d61318a9cd18f8c982529e587d2b64db8c
Sourabh Singh Rawat <Sourabh.rawatcc@gmail.com>
2021-11-05 03:35:09 +0530

    Added method to check if list is palindrome using stack

--- LinkedList/SinglyLinkedList/Medium/ReverseLinkedList.java
+++ LinkedList/SinglyLinkedList/Medium/ReverseLinkedList.java
@@ -1,10 +1,9 @@
+import java.util.Stack;
+
 class ReversedLinkedList {
     public static void main(String[] args) {
         // list
         LinkedList list = new LinkedList();
-        push(list, 7);
-        push(list, 6);
-        push(list, 5);
         push(list, 4);
         push(list, 3);
         push(list, 2);
@@ -15,6 +14,17 @@ class ReversedLinkedList {
         reversal(list);
         System.out.println("\nReversed List: ");
         print(list);
+
+        LinkedList list2 = new LinkedList();
+        push(list2, 1);
+        push(list2, 2);
+        push(list2, 3);
+        push(list2, 2);
+        push(list2, 1);
+
+        System.out.println("\n\nList 2");
+        print(list2);
+        System.out.println(isPalindrome(list2) ? "is palindrome" : "is not Palindrome");
     }
 
     static class LinkedList {
@@ -68,5 +78,31 @@ class ReversedLinkedList {
             current = next;
         }
         list.head = prev;
+    }
+
+    static boolean isPalindrome(LinkedList list) {
+        // if list empty return
+        if (list.head == null)
+            return false;
+
+        // using stacks
+        Stack<Integer> stack = new Stack<Integer>();
+        // iterate over list and storing in stack
+        Node cur = list.head;
+        while (cur != null) {
+            stack.push(cur.val);
+            cur = cur.next;
+        }
+
+        cur = list.head;
+        while (cur != null) {
+            int val = stack.pop();
+            if (cur.val != val) {
+                return false;
+            }
+            cur = cur.next;
+        }
+
+        return true;
     }
 }
