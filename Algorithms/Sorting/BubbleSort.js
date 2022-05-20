de8d9b80160a23f30a06d178c07e1e82820c24d2
Sourabh Singh Rawat <Sourabh.rawatcc@gmail.com>
2022-05-20 19:30:10 +0530

    Optimized bubble sort.

--- Algorithms/Sorting/BubbleSort.js
+++ Algorithms/Sorting/BubbleSort.js
@@ -1,16 +1,22 @@
-let arr = [ 5, 4, 3, 2, 1 ];
+let arr = [5, 4, 3, 2, 1];
 
 const bubbleSort = (arr) => {
-	for (let j = 0; j < arr.length; j++) {
-		for (let i = 0; i < arr.length - 1 - j; i++) {
-			if (arr[i] > arr[i + 1]) {
-				let temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
-			}
-		}
-	}
+  let swapped = false;
+
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length - j - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+
+    if (!swapped) break;
+  }
 };
 
+console.log("Array:", arr);
 bubbleSort(arr);
-console.log(arr);
+console.log("Bubble Sorted Array:", arr);
